c37d134a0f12176a559221842345052b7f4b96ae
ridomin <ridomin@live.com>
2022-09-12 00:23:10 -0700

    Enable All Telemetries, add pi-sense-hat sample

--- src/MQTTnet.Extensions.MultiCloud.BrokerIoTClient/PnPMqttClient.cs
+++ src/MQTTnet.Extensions.MultiCloud.BrokerIoTClient/PnPMqttClient.cs
@@ -1,21 +1,27 @@
 ﻿using MQTTnet.Client;
 using MQTTnet.Extensions.MultiCloud.Connections;
+using System.Threading.Tasks;
+using System.Threading;
 
 namespace MQTTnet.Extensions.MultiCloud.BrokerIoTClient
 {
     public class PnPMqttClient
     {
         public IMqttClient Connection { get; private set; }
+        public string InitialState { get; set; }
         public PnPMqttClient(IMqttClient c, string modelId = "")
         {
             Connection = c;
-            Connection.PublishStringAsync(
+            var pubAck = Connection.PublishStringAsync(
                 BirthConvention.BirthTopic(Connection.Options.ClientId),
                 new BirthConvention.BirthMessage(BirthConvention.ConnectionStatus.online)
                 {
                     ModelId = modelId
                 }.ToJson(),
-                Protocol.MqttQualityOfServiceLevel.AtLeastOnce, true).Wait();
+                Protocol.MqttQualityOfServiceLevel.AtLeastOnce, true).Result;
+            InitialState = string.Empty;
         }
+        public Task<MqttClientPublishResult> SendTelemetryAsync(object payload, CancellationToken t = default) => 
+            Connection.PublishStringAsync($"pnp/{Connection.Options.ClientId}/telemetry", Json.Stringify(payload), Protocol.MqttQualityOfServiceLevel.AtLeastOnce, false, t);
     }
 }
