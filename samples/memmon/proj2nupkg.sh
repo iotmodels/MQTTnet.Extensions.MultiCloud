c07b4b0e1f63ae48a2da94e52b24e2172b104822
ridomin <ridomin@live.com>
2022-09-12 21:21:29 -0700

    restore

--- samples/memmon/proj2nupkg.sh
+++ samples/memmon/proj2nupkg.sh
@@ -3,6 +3,6 @@ dotnet remove reference ../../src/MQTTnet.Extensions.MultiCloud.AwsIoTClient/MQT
 dotnet remove reference ../../src/MQTTnet.Extensions.MultiCloud.AzureIoTClient/MQTTnet.Extensions.MultiCloud.AzureIoTClient.csproj
 dotnet remove reference ../../src/MQTTnet.Extensions.MultiCloud.BrokerIoTClient/MQTTnet.Extensions.MultiCloud.BrokerIoTClient.csproj
 
-dotnet add package MQTTnet.Extensions.MultiCloud.AzureIoTClient  -s https://www.myget.org/F/ridopackages/api/v3/index.json
-dotnet add package MQTTnet.Extensions.MultiCloud.BrokerIoTClient json -s https://www.myget.org/F/ridopackages/api/v3/index.json
-dotnet add package MQTTnet.Extensions.MultiCloud.AwsIoTClient  -s https://www.myget.org/F/ridopackages/api/v3/index.json
+dotnet add package MQTTnet.Extensions.MultiCloud.AzureIoTClient  
+dotnet add package MQTTnet.Extensions.MultiCloud.BrokerIoTClient  
+dotnet add package MQTTnet.Extensions.MultiCloud.AwsIoTClient
