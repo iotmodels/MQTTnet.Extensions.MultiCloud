9f83fc7fd6e732a0dc1a98eee6aa62f86c6d6aef
rido-min <rido-min@users.noreply.github.com>
2022-09-12 19:32:00 -0700

    dotnet add 2 sources

--- samples/memmon/proj2nupkg.sh
+++ samples/memmon/proj2nupkg.sh
@@ -3,6 +3,6 @@ dotnet remove reference ../../src/MQTTnet.Extensions.MultiCloud.AwsIoTClient/MQT
 dotnet remove reference ../../src/MQTTnet.Extensions.MultiCloud.AzureIoTClient/MQTTnet.Extensions.MultiCloud.AzureIoTClient.csproj
 dotnet remove reference ../../src/MQTTnet.Extensions.MultiCloud.BrokerIoTClient/MQTTnet.Extensions.MultiCloud.BrokerIoTClient.csproj
 
-dotnet add package MQTTnet.Extensions.MultiCloud.AzureIoTClient -s https://www.myget.org/F/ridopackages/api/v3/index.json
-dotnet add package MQTTnet.Extensions.MultiCloud.BrokerIoTClient -s https://www.myget.org/F/ridopackages/api/v3/index.json
-dotnet add package MQTTnet.Extensions.MultiCloud.AwsIoTClient -s https://www.myget.org/F/ridopackages/api/v3/index.json
+dotnet add package MQTTnet.Extensions.MultiCloud.AzureIoTClient -s https://api.nuget.org/v3/index.json -s https://www.myget.org/F/ridopackages/api/v3/index.json
+dotnet add package MQTTnet.Extensions.MultiCloud.BrokerIoTClient -s https://api.nuget.org/v3/index.json -s https://www.myget.org/F/ridopackages/api/v3/index.json
+dotnet add package MQTTnet.Extensions.MultiCloud.AwsIoTClient -s https://api.nuget.org/v3/index.json -s https://www.myget.org/F/ridopackages/api/v3/index.json
