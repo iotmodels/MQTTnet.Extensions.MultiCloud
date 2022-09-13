85b73f250a0ee8951f4e65b45fe1393d5eb6e644
rido-min <rido-min@users.noreply.github.com>
2022-09-12 19:28:48 -0700

    fix prj restore

--- samples/memmon/proj2nupkg.sh
+++ samples/memmon/proj2nupkg.sh
@@ -1,3 +1,4 @@
+
 dotnet remove reference ../../src/MQTTnet.Extensions.MultiCloud.AwsIoTClient/MQTTnet.Extensions.MultiCloud.AwsIoTClient.csproj 
 dotnet remove reference ../../src/MQTTnet.Extensions.MultiCloud.AzureIoTClient/MQTTnet.Extensions.MultiCloud.AzureIoTClient.csproj
 dotnet remove reference ../../src/MQTTnet.Extensions.MultiCloud.BrokerIoTClient/MQTTnet.Extensions.MultiCloud.BrokerIoTClient.csproj
