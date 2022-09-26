c8955822afe97968f419adfd7446bb1edc6156f4
ridomin <ridomin@live.com>
2022-09-26 14:46:37 -0700

    start next

---
+++ samples/iot-device/Device.cs
@@ -0,0 +1,21 @@
+namespace iot_device
+{
+    public class Device : BackgroundService
+    {
+        private readonly ILogger<Device> _logger;
+
+        public Device(ILogger<Device> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
