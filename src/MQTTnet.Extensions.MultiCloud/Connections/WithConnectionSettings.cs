9bb86b220b62935ea6b881df61b7f2a22e04fe88
ridomin <ridomin@live.com>
2022-09-29 18:04:49 -0700

    clean ns

--- src/MQTTnet.Extensions.MultiCloud/Connections/WithConnectionSettings.cs
+++ src/MQTTnet.Extensions.MultiCloud/Connections/WithConnectionSettings.cs
@@ -1,41 +1,40 @@
 ﻿using MQTTnet.Client;
 
-namespace MQTTnet.Extensions.MultiCloud.Connections
+namespace MQTTnet.Extensions.MultiCloud.Connections;
+
+public static partial class MqttNetExtensions
 {
-    public static partial class MqttNetExtensions
+    public static MqttClientOptionsBuilder WithConnectionSettings(this MqttClientOptionsBuilder builder, ConnectionSettings cs, bool withLWT = false)
     {
-        public static MqttClientOptionsBuilder WithConnectionSettings(this MqttClientOptionsBuilder builder, ConnectionSettings cs, bool withLWT = false)
+        builder
+            .WithTimeout(TimeSpan.FromSeconds(30))
+            .WithTcpServer(cs.HostName, cs.TcpPort)
+            .WithKeepAlivePeriod(TimeSpan.FromSeconds(cs.KeepAliveInSeconds))
+            .WithCleanSession(cs.CleanSession)
+            .WithTlsSettings(cs);
+
+        if (!string.IsNullOrEmpty(cs.Password))
+        {
+            builder.WithCredentials(cs.UserName, cs.Password);
+        }
+
+        if (cs.ClientId == "{machineName}")
+        {
+            cs.ClientId = Environment.MachineName;
+        }
+
+        builder.WithClientId(cs.ClientId);
+
+        if (withLWT)
         {
             builder
-                .WithTimeout(TimeSpan.FromSeconds(30))
-                .WithTcpServer(cs.HostName, cs.TcpPort)
-                .WithKeepAlivePeriod(TimeSpan.FromSeconds(cs.KeepAliveInSeconds))
-                .WithCleanSession(cs.CleanSession)
-                .WithTlsSettings(cs);
-
-            if (!string.IsNullOrEmpty(cs.Password))
-            {
-                builder.WithCredentials(cs.UserName, cs.Password);
-            }
-
-            if (cs.ClientId == "{machineName}")
-            {
-                cs.ClientId = Environment.MachineName;
-            }
-
-            builder.WithClientId(cs.ClientId);
-
-            if (withLWT)
-            {
-                builder
-                .WithWillTopic(BirthConvention.BirthTopic(cs.ClientId!))
-                .WithWillQualityOfServiceLevel(Protocol.MqttQualityOfServiceLevel.AtLeastOnce)
-                .WithWillPayload(BirthConvention.LastWillPayload(cs.ModelId!))
-                .WithWillRetain(true);
-            }
-
-            return builder;
+            .WithWillTopic(BirthConvention.BirthTopic(cs.ClientId!))
+            .WithWillQualityOfServiceLevel(Protocol.MqttQualityOfServiceLevel.AtLeastOnce)
+            .WithWillPayload(BirthConvention.LastWillPayload(cs.ModelId!))
+            .WithWillRetain(true);
         }
+
+        return builder;
     }
 }
 
--- src/MQTTnet.Extensions.MultiCloud/SubscribeExtension.cs
+++ src/MQTTnet.Extensions.MultiCloud/SubscribeExtension.cs
@@ -1,32 +1,31 @@
 ﻿using MQTTnet.Client;
 using System.Diagnostics;
 
-namespace MQTTnet.Extensions.MultiCloud
+namespace MQTTnet.Extensions.MultiCloud;
+
+public static class SubscribeExtension
 {
-    public static class SubscribeExtension
+    private static readonly List<string> subscriptions = new List<string>();
+
+    public static async Task SubscribeWithReplyAsync(this IMqttClient client, string topic, CancellationToken cancellationToken = default)
     {
-        private static readonly List<string> subscriptions = new List<string>();
-
-        public static async Task SubscribeWithReplyAsync(this IMqttClient client, string topic, CancellationToken cancellationToken = default)
+        if (!subscriptions.Contains(topic))
         {
-            if (!subscriptions.Contains(topic))
-            {
-                subscriptions.Add(topic);
+            subscriptions.Add(topic);
 
-                var subAck = await client.SubscribeAsync(new MqttClientSubscribeOptionsBuilder().WithTopicFilter(topic).Build(), cancellationToken);
-                subAck.TraceErrors();
+            var subAck = await client.SubscribeAsync(new MqttClientSubscribeOptionsBuilder().WithTopicFilter(topic).Build(), cancellationToken);
+            subAck.TraceErrors();
 
-            }
         }
+    }
 
-        public static void ReSuscribe(this IMqttClient client)
+    public static void ReSuscribe(this IMqttClient client)
+    {
+        subscriptions.ForEach(async t =>
         {
-            subscriptions.ForEach(async t =>
-            {
-                Trace.TraceInformation($"Re-Subscribing to {t}");
-                var subAck = await client.SubscribeAsync(t);
-                subAck.TraceErrors();
-            });
-        }
+            Trace.TraceInformation($"Re-Subscribing to {t}");
+            var subAck = await client.SubscribeAsync(t);
+            subAck.TraceErrors();
+        });
     }
 }
