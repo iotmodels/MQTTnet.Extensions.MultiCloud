1540b571e54bef4fea79483949d32a1f7127afe4
ridomin <ridomin@live.com>
2022-10-04 09:44:17 -0700

    rm dupe task timeout

--- src/MQTTnet.Extensions.MultiCloud/TaskTimeoutExtension.cs
+++ src/MQTTnet.Extensions.MultiCloud/TaskTimeoutExtension.cs
@@ -1,10 +1,17 @@
-﻿namespace MQTTnet.Extensions.MultiCloud;
+﻿using System.Diagnostics;
+
+namespace MQTTnet.Extensions.MultiCloud;
 
 public static class TaskTimeoutExtension
 {
     public static async Task<T> TimeoutAfter<T>(this Task<T> source, TimeSpan timeout)
     {
-        if (await Task.WhenAny(source, Task.Delay(timeout)) != source)
+        var actualTimeout = timeout;
+        if (Debugger.IsAttached)
+        {
+            actualTimeout = timeout.Add(TimeSpan.FromSeconds(300));
+        }
+        if (await Task.WhenAny(source, Task.Delay(actualTimeout)) != source)
         {
             throw new TimeoutException();
         }
