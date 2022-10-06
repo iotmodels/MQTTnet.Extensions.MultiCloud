2cd537d9f2e6044d564b4fb658cfcefa03effd17
ridomin <ridomin@live.com>
2022-10-05 17:39:24 -0700

    empty commands

--- src/MQTTnet.Extensions.MultiCloud/ICommand.cs
+++ src/MQTTnet.Extensions.MultiCloud/ICommand.cs
@@ -1,3 +1,7 @@
 ﻿namespace MQTTnet.Extensions.MultiCloud;
 
+public interface ICommand : ICloudToDevice<string, string> { }
+
+public interface ICommand<T> : ICloudToDevice<T, string> { }
+
 public interface ICommand<T, TResp> : ICloudToDevice<T, TResp> { }
