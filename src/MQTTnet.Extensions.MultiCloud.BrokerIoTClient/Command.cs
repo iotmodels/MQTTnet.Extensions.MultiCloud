adc0cab55aa0a90ade9793796692837a56bd7f83
ridomin <ridomin@live.com>
2022-10-05 17:39:24 -0700

    empty commands

--- src/MQTTnet.Extensions.MultiCloud.BrokerIoTClient/Command.cs
+++ src/MQTTnet.Extensions.MultiCloud.BrokerIoTClient/Command.cs
@@ -12,3 +12,24 @@ public class Command<T, TResp> : CloudToDeviceBinder<T, TResp>, ICommand<T, TRes
         ResponseTopicPattern = "device/{clientId}/commands/{name}/resp";
     }
 }
+
+public class Command<T> : CloudToDeviceBinder<T, string>, ICommand<T>
+{
+    public Command(IMqttClient client, string name)
+        : base(client, name)
+    {
+        RequestTopicPattern = "device/{clientId}/commands/{name}";
+        ResponseTopicPattern = "device/{clientId}/commands/{name}/resp";
+    }
+}
+
+public class Command : CloudToDeviceBinder<string, string>, ICommand
+{
+    public Command(IMqttClient client, string name)
+        : base(client, name)
+    {
+        RequestTopicPattern = "device/{clientId}/commands/{name}";
+        ResponseTopicPattern = "device/{clientId}/commands/{name}/resp";
+    }
+}
+
---
+++ tests/MQTTnet.Extensions.MultiCloud.UnitTests/BrokerJsonBindings/CommandBinderFixture.cs
@@ -0,0 +1,82 @@
+﻿using MQTTnet.Extensions.MultiCloud.BrokerIoTClient;
+using MQTTnet.Extensions.MultiCloud.Serializers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MQTTnet.Extensions.MultiCloud.UnitTests.BrokerJsonBindings
+{
+    public class CommandBinderFixture
+    {
+        [Fact]
+        public void CommandWithReqResp()
+        {
+            MockMqttClient mockMqtt = new();
+            Command<int, string> cmd = new(mockMqtt, "aCmdReqResp");
+            bool cmdReceived = false;
+            cmd.OnMessage = async req =>
+            {
+                cmdReceived = true;
+                return await Task.FromResult(req.ToString());
+            };
+            mockMqtt.SimulateNewBinaryMessage("device/mock/commands/aCmdReqResp",new UTF8JsonSerializer().ToBytes(2));
+            Assert.True(cmdReceived);
+            Assert.Equal("device/mock/commands/aCmdReqResp/resp", mockMqtt.topicRecceived);
+            Assert.Equal("\"2\"", mockMqtt.payloadReceived);
+        }
+
+        [Fact]
+        public void CommandWithReq()
+        {
+            MockMqttClient mockMqtt = new();
+            Command<int> cmd = new(mockMqtt, "aCmdReq");
+            bool cmdReceived = false;
+            cmd.OnMessage = async req =>
+            {
+                cmdReceived = true;
+                return await Task.FromResult(string.Empty);
+            };
+            mockMqtt.SimulateNewBinaryMessage("device/mock/commands/aCmdReq", new UTF8JsonSerializer().ToBytes(2));
+            Assert.True(cmdReceived);
+            Assert.Equal("device/mock/commands/aCmdReq/resp", mockMqtt.topicRecceived);
+            Assert.Equal("\"\"", mockMqtt.payloadReceived);
+        }
+
+        [Fact]
+        public void CommandWithRes()
+        {
+            MockMqttClient mockMqtt = new();
+            Command<string, int> cmd = new(mockMqtt, "aCmdRes");
+            bool cmdReceived = false;
+            cmd.OnMessage = async req =>
+            {
+                cmdReceived = true;
+                return await Task.FromResult(1);
+            };
+            mockMqtt.SimulateNewBinaryMessage("device/mock/commands/aCmdRes", new UTF8JsonSerializer().ToBytes(""));
+            Assert.True(cmdReceived);
+            Assert.Equal("device/mock/commands/aCmdRes/resp", mockMqtt.topicRecceived);
+            Assert.Equal("1", mockMqtt.payloadReceived);
+        }
+
+        [Fact]
+        public void CommandEmpty()
+        {
+            MockMqttClient mockMqtt = new();
+            Command cmd = new(mockMqtt, "aCmd");
+            bool cmdReceived = false;
+            cmd.OnMessage = async req =>
+            {
+                cmdReceived = true;
+                return await Task.FromResult(string.Empty);
+            };
+            mockMqtt.SimulateNewBinaryMessage("device/mock/commands/aCmd", new UTF8JsonSerializer().ToBytes(""));
+            Assert.True(cmdReceived);
+            Assert.Equal("device/mock/commands/aCmd/resp", mockMqtt.topicRecceived);
+            Assert.Equal("\"\"", mockMqtt.payloadReceived);
+        }
+    }
+}
