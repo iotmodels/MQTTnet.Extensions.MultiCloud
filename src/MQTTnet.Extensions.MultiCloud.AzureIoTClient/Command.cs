3e989e619411a1c69b84d797fb5bc9b955ba4a5e
ridomin <ridomin@live.com>
2022-10-05 17:43:55 -0700

    Add empty commands to hub binders

--- src/MQTTnet.Extensions.MultiCloud.AzureIoTClient/Command.cs
+++ src/MQTTnet.Extensions.MultiCloud.AzureIoTClient/Command.cs
@@ -12,3 +12,23 @@ public class Command<T, TResp> : CloudToDeviceBinder<T, TResp>, ICommand<T, TRes
         ResponseTopicPattern = "$iothub/methods/res/200/?$rid={rid}";
     }
 }
+
+public class Command<T> : CloudToDeviceBinder<T, string>, ICommand<T>
+{
+    public Command(IMqttClient client, string name)
+    : base(client, name)
+    {
+        RequestTopicPattern = "$iothub/methods/POST/{name}/#";
+        ResponseTopicPattern = "$iothub/methods/res/200/?$rid={rid}";
+    }
+}
+
+public class Command : CloudToDeviceBinder<string, string>, ICommand
+{
+    public Command(IMqttClient client, string name)
+    : base(client, name)
+    {
+        RequestTopicPattern = "$iothub/methods/POST/{name}/#";
+        ResponseTopicPattern = "$iothub/methods/res/200/?$rid={rid}";
+    }
+}
