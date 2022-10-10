516d8b44673f5a1eb4d814639175af4e477b5ca9
ridomin <ridomin@live.com>
2022-10-10 12:10:51 -0700

    2nd telemetry

--- samples/memmon/dtmi_rido_memmon-2.mqtt.g.cs
+++ samples/memmon/dtmi_rido_memmon-2.mqtt.g.cs
@@ -15,6 +15,7 @@ public class memmon : Imemmon
     public IWritableProperty<bool> Property_enabled { get; set; }
     public IWritableProperty<int> Property_interval { get; set; }
     public ITelemetry<double> Telemetry_workingSet { get; set; }
+    public ITelemetry<double> Telemetry_managedMemory { get; set; }
     public ICommand<DiagnosticsMode, Dictionary<string, string>> Command_getRuntimeStats { get; set; }
     public ICommand<int, bool> Command_isPrime { get; set; }
     public ICommand<int> Command_malloc { get; set; }
@@ -27,6 +28,7 @@ public class memmon : Imemmon
         Property_interval = new WritableProperty<int>(c, "interval");
         Property_enabled = new WritableProperty<bool>(c, "enabled");
         Telemetry_workingSet = new Telemetry<double>(c, "workingSet");
+        Telemetry_managedMemory = new Telemetry<double>(c, "managedMemory");
         Command_getRuntimeStats = new Command<DiagnosticsMode, Dictionary<string, string>>(c, "getRuntimeStats");
         Command_isPrime = new Command<int, bool>(c, "isPrime");
         Command_malloc = new Command<int>(c, "malloc");
