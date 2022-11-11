026bdb869219ada145e07ac5372d2d787ba97aeb
ridomin <ridomin@live.com>
2022-11-11 08:01:11 -0800

    copy serializers to samples

---
+++ samples/mqtt-grpc-device/Serializers/TelemetryProtobuf.cs
@@ -0,0 +1,20 @@
+﻿using MQTTnet.Client;
+using MQTTnet.Extensions.MultiCloud;
+using MQTTnet.Extensions.MultiCloud.Binders;
+
+namespace mqtt_grpc_device.Serializers;
+
+public class TelemetryProtobuf<T> : DeviceToCloudBinder<T>, ITelemetry<T>
+{
+
+    public TelemetryProtobuf(IMqttClient mqttClient) :
+      this(mqttClient, string.Empty)
+    { }
+
+    public TelemetryProtobuf(IMqttClient mqttClient, string name)
+        : base(mqttClient, name, new ProtobufSerializer())
+    {
+        TopicPattern = "devices/{clientId}/messages/events/";
+        WrapMessage = false;
+    }
+}
