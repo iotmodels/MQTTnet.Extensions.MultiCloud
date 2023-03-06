2500b799ab2f6d0ca2b5dd85961334287bfdaa8f
Rido <ridomin@live.com>
2023-03-06 15:06:21 -0800

    Add support for EdgeModules (#98)
    * support edge modules
    
    * adding tests
    
    ---------
    
    Co-authored-by: rido-min <rido-min@users.noreply.github.com>

--- tests/MQTTnet.Extensions.MultiCloud.UnitTests/BirthConventionFixture.cs
+++ tests/MQTTnet.Extensions.MultiCloud.UnitTests/BirthConventionFixture.cs
@@ -11,7 +11,7 @@ namespace MQTTnet.Extensions.MultiCloud.UnitTests
             //var json = @"{""model-id"":""dtmi:rido:pnp:memmon;1"",""when"":""2022-09-11T16:28:38.1615346-07:00"",""status"":""offline""}";
             var json = @"{""model-id"":""dtmi:rido:pnp:sensehat;1"",""when"":""2023-02-08T08:39:40.9008917+00:00"",""status"":""online""}";
             BirthConvention.BirthMessage bm = Json.FromString<BirthConvention.BirthMessage>(json)!;
-            Assert.Equal(BirthConvention.ConnectionStatus.offline, bm.ConnectionStatus);
+            Assert.Equal(BirthConvention.ConnectionStatus.online, bm.ConnectionStatus);
         }
     }
 }
